aa97994e8c486b56636733d1f98f27ca452d2883
Devin Tyler Cunningham <dtyler.cunningham@gmail.com>
2025-09-01 12:30:14 -0700

    stuff

--- loader.js
+++ loader.js
@@ -4,9 +4,9 @@ export default function myImageLoader({ src, width, quality }) {
     const isLocal = !src.startsWith('http');
     const query = new URLSearchParams();
 
-    const imageOptimizationApi = '{change this to next-image-transformation container}';
+    const imageOptimizationApi = 'http://img.devincuningham.com';
     // Your NextJS application URL
-    const baseUrl = '{change this to exif-photo-blog container}';
+    const baseUrl = 'photo.devincunningham.com';
 
     const fullSrc = `${baseUrl}${src}`;
 
